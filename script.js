fc7d572bc3b5884a568b3f258fdf64b72c3661e2
Vincent <vicentebatero@gmail.com>
2022-08-29 08:46:42 -0400

    Function to compare choices and determine a winner.

--- script.js
+++ script.js
@@ -6,4 +6,43 @@ function getComputerChoice() {
     return choice
 }
 
-console.log(getComputerChoice())
+// compare both choices and determine a winner.
+function playRound(playerSelection, computerSelection) {
+    switch (playerSelection) {
+        case computerSelection:
+            return "It is a tie"
+            break
+        case "rock":
+            switch (computerSelection) {
+                case "paper":
+                    return "You lost! Rock is beaten by paper."
+                    break
+                case "scissors":
+                    return "You win! Rock beats scissors."
+                    break
+            }
+        case "paper":
+            switch (computerSelection) {
+                case "scissors":
+                    return "You lost! Paper is beaten by scissors."
+                    break
+                case "rock":
+                    return "You win! Paper beats rock."
+                    break
+            }
+        case "scissors":
+            switch (computerSelection) {
+                case "rock":
+                    return "You lost! Scissors is beaten by rock"
+                    break
+                case "paper":
+                    return "You win! Scissors beats paper"
+                    break
+            }
+    }
+}
+
+
+let player = prompt("What is your bet, Rock, Paper or Scissors?").toLowerCase()
+
+console.log(playRound(player, getComputerChoice()))
